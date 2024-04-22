56128b0a2f8fbae9eea14246bbd45ce7f8324726
Christine Rose Riego <127263377+aespasol@users.noreply.github.com>
2024-04-22 19:12:07 +0800

    initial commit

---
+++ functions/api.js
@@ -0,0 +1,26 @@
+
+const express = require("express");
+const serverless = require("serverless-http");
+const router = require("./routes/author");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const dbCloudUrl = 
+"mongodb+srv://christineroseriego0125:riegochristine12345@cluster0.5rt9ogp.mongodb.net/mongodb+srv://christineroseriego0125:QfShdkWnW0k8YwoB@cluster0.5rt9ogp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(dbCloudUrl)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+
+app.use("/.netlify/functions/api", router);
+module.exports.handler = serverless(app);
